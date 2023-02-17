16d81fda7ae9514295a959b8b58c6a10930a437c
wangbei16 <wangbei6@jd.com>
2023-02-17 18:31:46 +0800

    taro textarea disabled 和 readonly 不能同时设置

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,19 @@
 import { View } from '@tarojs/components'
+import './index.scss'
 
 function Index() {
   return (
     <View >
-      <View>
-        按照 React 的使用方式，maxLength={5} 不生效
-      </View>
-      <input className='input' maxLength={5}/>
-      <View>
-        不按照 React 的使用方式，maxlength={5} 生效, 但是有类型错误
-      </View>
-      <input className='input' maxlength={5}/>
+      {/*<View>*/}
+      {/*  按照 React 的使用方式，maxLength={5} 不生效*/}
+      {/*</View>*/}
+      {/*<input className='input' maxLength={5}/>*/}
+      {/*<View>*/}
+      {/*  不按照 React 的使用方式，maxlength={5} 生效, 但是有类型错误*/}
+      {/*</View>*/}
+      {/*<input className='input' maxlength={5}/>*/}
+
+      <textarea className='input' value={'ssdfdsfsdfdf'} name="sd" disabled={true} readOnly={false} id="" cols="30" rows="10"></textarea>
     </View>
   )
 }
